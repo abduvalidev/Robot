033827c7ca342d4066003e708b7d663b6b9c5116
Abduvalidev <abduvalidev1@gmail.com>
2024-12-21 20:03:26 +0500

    false

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: false,
+  reactStrictMode: false, // React strict mode-ni o'chirish
+  webpack(config, { isServer }) {
+    // Serverda bo'lmagan hollarda 'fs' va 'path' modullarini o'chirish
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
